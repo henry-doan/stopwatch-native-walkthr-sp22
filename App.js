d7d0badf98ff3811c168b0e3281caa49236da7be
Henry Doan <henrydoan96@gmail.com>
2022-04-14 09:41:28 -0600

    final

--- App.js
+++ App.js
@@ -7,10 +7,7 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-} from 'react-native';
+import { SafeAreaView, } from 'react-native';
 import StopWatch from './src/screens/Stopwatch';
 
 const App = () => (
